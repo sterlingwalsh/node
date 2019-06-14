78513f47446118524c4e502b4adbe94ad4626c10
sterling.walsh.dev@gmail.com <sterling.walsh.dev@gmail.com>
2019-06-14 16:17:22 -0400

    express section

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const http = require('http');
-const routes = require('./routes');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin').router;
+const shopRoutes = require('./routes/shop').router;
 
 const localPort = 3000;
 
-const server = http.createServer(routes.requestHandler);
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
-server.listen(localPort);
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
+app.listen(localPort);
